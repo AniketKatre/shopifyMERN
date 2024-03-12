d99acd535197f5325f6f4bd3a120dfd969007e2f
AniketKatre <aniketkatre3@gmail.com>
2024-03-12 23:47:04 +0530

    create Backend with nodejs and fetch data using axios in frontend and build dev pack..... 12Mar2024

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// default common js  type : below method commly sed in js
+// const express = require('express')
+// type= module ES6 and used also in overall project
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("App is running.....")
+});
+
+app.get('/api/products', (req, res) =>{
+    res.json(products)
+});
+
+app.get('/api/products/:id', (req, res) =>{
+    const product = products.find((p) => p._id===req.params.id);
+    res.json(product)
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on port:${port}`)
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
@@ -12,7 +13,7 @@ const App = () => {
 
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+            <Outlet />
         </Container>
       </main>
 
