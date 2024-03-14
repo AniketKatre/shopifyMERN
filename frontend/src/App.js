4b84ba3af0cf407143dad08dbb067a7ad13b42e2
AniketKatre <aniketkatre3@gmail.com>
2024-03-14 22:51:37 +0530

    Redux Toolkit Setup and State Management and get data from slices and remove axios, useState  & useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-    <Header />
+      <Header />
 
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-            <Outlet />
+          <Outlet />
         </Container>
       </main>
 
-    <Footer />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
